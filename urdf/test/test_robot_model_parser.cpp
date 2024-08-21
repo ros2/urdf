7843a26e4f0645b6d7a563562a7ff7edf6728fd9
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-08-21 10:58:31 +0200

    Enable test_robot_model_parser test
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- urdf/test/test_robot_model_parser.cpp
+++ urdf/test/test_robot_model_parser.cpp
@@ -41,10 +41,7 @@
 #include "gtest/gtest.h"
 #include "urdf/model.h"
 
-int g_argc;
-char ** g_argv;
-
-class TestParser : public testing::Test
+class TestParser : public testing::TestWithParam<std::vector<std::string>>
 {
 public:
   bool checkModel(urdf::Model & robot)
@@ -109,11 +106,12 @@ protected:
   size_t num_links;
 };
 
-TEST_F(TestParser, test) {
-  ASSERT_GE(g_argc, 3);
-  std::string folder = std::string(g_argv[1]) + "/test/";
+TEST_P(TestParser, test) {
+  std::vector<std::string> const & input = GetParam();
+
+  std::string folder = _TEST_RESOURCES_DIR_PATH;
   fprintf(stderr, "Folder %s\n", folder.c_str());
-  std::string file = std::string(g_argv[2]);
+  std::string file = std::string(input[0]);
   bool expect_success = (file.substr(0, 5) != "fail_");
   urdf::Model robot;
   fprintf(stderr, "Parsing file %s, expecting %d\n", (folder + file).c_str(), expect_success);
@@ -122,11 +120,10 @@ TEST_F(TestParser, test) {
     return;
   }
 
-  ASSERT_EQ(g_argc, 7);
-  std::string robot_name = std::string(g_argv[3]);
-  std::string root_name = std::string(g_argv[4]);
-  size_t expected_num_joints = atoi(g_argv[5]);
-  size_t expected_num_links = atoi(g_argv[6]);
+  std::string robot_name = std::string(input[1]);
+  std::string root_name = std::string(input[2]);
+  size_t expected_num_joints = atoi(input[3].c_str());
+  size_t expected_num_links = atoi(input[4].c_str());
 
   ASSERT_TRUE(robot.initFile(folder + file));
 
@@ -142,15 +139,27 @@ TEST_F(TestParser, test) {
   EXPECT_EQ(robot.links_.size(), expected_num_links);
 
   // test reading from parameter server
-  ASSERT_TRUE(robot.initParam("robot_description"));
-  ASSERT_FALSE(robot.initParam("robot_description_wim"));
-  SUCCEED();
+  // ASSERT_TRUE(robot.initParam("robot_description"));
+  // ASSERT_FALSE(robot.initParam("robot_description_wim"));
 }
+
+INSTANTIATE_TEST_CASE_P(GroupTestParser, TestParser, ::testing::Values(
+  std::vector<std::string>({"test_robot.urdf", "r2d2", "dummy_link", "16", "17"}),
+  std::vector<std::string>({"no_visual.urdf", "no_visual", "link1", "0", "1"}),
+  std::vector<std::string>({"one_link.urdf", "one_link", "link1", "0", "1"}),
+  std::vector<std::string>({"two_links_one_joint.urdf", "two_links_one_joint", "link1", "1", "2"}),
+  // Cases expected not to parse correctly only get filename information (path, urdf file)
+  std::vector<std::string>({"fail_pr2_desc_bracket.urdf"}),
+  std::vector<std::string>({"fail_three_links_one_joint.urdf"}),
+  std::vector<std::string>({"fail_pr2_desc_double_joint.urdf"}),
+  std::vector<std::string>({"fail_pr2_desc_loop.urdf"}),
+  std::vector<std::string>({"fail_pr2_desc_no_filename_in_mesh.urdf"}),
+  std::vector<std::string>({"fail_pr2_desc_no_joint2.urdf"}),
+  std::vector<std::string>({"fail_pr2_desc_two_trees.urdf"})
+));
 
 int main(int argc, char ** argv)
 {
   testing::InitGoogleTest(&argc, argv);
-  g_argc = argc;
-  g_argv = argv;
   return RUN_ALL_TESTS();
 }
